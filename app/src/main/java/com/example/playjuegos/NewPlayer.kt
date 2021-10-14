bb2a08d25290ffd97ed7943ee5d4b435acad9ead
Prades <alexandre.prades.c@gmail.com>
2021-10-14 20:26:05 +0200

    v3.1

--- app/src/main/java/com/example/playjuegos/NewPlayer.kt
+++ app/src/main/java/com/example/playjuegos/NewPlayer.kt
@@ -2,10 +2,32 @@ package com.example.playjuegos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class NewPlayer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_player)
+
+        val telefonos = arrayOf ("Telefono1", "Telefono2", "Telefono3", "Telefono4","Telefono5")
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, telefonos)
+
+        val listaTelefonos = findViewById<Spinner>(R.id.spinner)
+        listaTelefonos.adapter = adaptador
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        listaTelefonos.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val telefono = findViewById<TextView>(R.id.editTextNumber)
+                if (parent != null) {
+                    telefono.text = parent.getItemAtPosition(position).toString()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
     }
 }
